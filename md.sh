796b64070d70d2bcf1914010fe9c151ad8f539ab
socd06 <socd06@gmail.com>
2020-10-25 12:39:41 -0400

    restore mdrun changes and git stash

--- md.sh
+++ md.sh
@@ -82,10 +82,10 @@ run-folders() {
 										gas-gas
 										# send to main network computer
 										scp -P 28 -r -C /home/test/lennard-jones-sims/results/rdf-$preffix-p$p-t$t.xvg test@148.247.198.140:/home/test/git/lennard-jones-sims/results
+										stash
 						fi
-							stash
-
 				done
+			done
 			done
 			cd ..
 	done
@@ -122,16 +122,16 @@ gas-gas(){
 
 run-commands() {
 	gmx grompp -f MDP/0_minim.mdp -c OUT/solv2.gro -p FF/topol.top -o OUT/p$p-t$t/0-em.tpr
-	gmx mdrun -v -deffnm OUT/p$p-t$t/0-em -GMX_USE_RDTSCP=ON
+	gmx mdrun -v -deffnm OUT/p$p-t$t/0-em
 	echo "saving to" OUT/p$p-t$t/0-em
 	gmx grompp -f MDP/0_nvt.mdp -c OUT/p$p-t$t/0-em.gro -p FF/topol.top -o OUT/p$p-t$t/1-nvt.tpr
-	gmx mdrun -v -deffnm OUT/p$p-t$t/1-nvt -GMX_USE_RDTSCP=ON
+	gmx mdrun -v -deffnm OUT/p$p-t$t/1-nvt
 	echo "saving to" OUT/p$p-t$t/1-nvt
 	gmx grompp -f MDP/0_npt.mdp -c OUT/p$p-t$t/1-nvt.gro -p FF/topol.top -o OUT/p$p-t$t/2-npt.tpr
-	gmx mdrun -v -deffnm OUT/p$p-t$t/2-npt -GMX_USE_RDTSCP=ON
+	gmx mdrun -v -deffnm OUT/p$p-t$t/2-npt
 	echo "saving to" OUT/p$p-t$t/2-npt
 	gmx grompp -f MDP/0_md.mdp -c OUT/p$p-t$t/2-npt.gro -p FF/topol.top -o OUT/p$p-t$t/3-md.tpr -maxwarn 1
-	gmx mdrun -v -deffnm OUT/p$p-t$t/3-md -GMX_USE_RDTSCP=ON
+	gmx mdrun -v -deffnm OUT/p$p-t$t/3-md
 	echo "saving to" OUT/p$p-t$t/3-md
 }
 
