d6bd32f8031235b8b3e2d946c3f56d3955f06b37
socd06 <socd06@gmail.com>
2020-10-29 21:19:17 -0400

    feat: update dev and md scripts

--- dev.sh
+++ dev.sh
@@ -1,110 +1,110 @@
 #!/bin/bash
 
-export BEGMD=3000
-export ENDMD=6000
-
 main() {
-	if source /usr/local/gromacs2016-3/bin/GMXRC
+	echo "Sourcing available version of GROMACS..."
+	if source /opt/gromacs/gromacs-2020.3-GPU/bin/GMXRC
 	then
-			echo "Exported GROMACS 2016"
-	else
-			source /usr/local/gromacs/bin/GMXRC
-			echo "Exported GROMACS 2020"
+			echo "Exported GPU-enabled GROMACS 2020.3"
+	elif source /opt/gromacs/gromacs-2019.6-GPU/bin/GMXRC
+	then
+			echo "Exported GPU-enabled GROMACS 2019-6"
+	elif source /usr/local/gromacs/bin/GMXRC
+	then
+			echo "Exported GROMACS"
+  elif source /usr/local/gromacs5-1-5/bin/GMXRC
+  then
+      echo "Exported GROMACS 5.1.5"
+	else source /usr/local/gromacs2016-3/bin/GMXRC
+			echo "Exported GROMACS 2016-3"
 	fi
 		run-folders
 }
 
 run-folders() {
-	for preffix in {2..2}
+	for preffix in {3..3}
 	do
 	  folder=$preffix*
 	  echo "going into" $folder
 	  cd $folder
 		echo "currently in"
 		pwd
-		for p in {1..100}
-		do
-			for t in {200..800..6}
+			for p in {1..100}
 			do
-				if python ../scripts/check_files.py
-				then
-					echo "Python3 File check succesfull"
-				else
-					echo "Python3 File check failed. Running Python2 version."
-					python ../scripts/check_p2.py
-				fi
-					if grep -Fxq "$preffix-p$p-t$t" ../iters.txt
+				for t in {200..800..6}
+				do
+					if python ../scripts/check_files.py
 					then
-							# code if found
-							echo "Simulation found in log. Skipping..."
-						else
-								# code if not found
-								echo "Not found. Simulating..."
+						echo "Python3 File check succesfull"
+					else
+						echo "Python3 File check failed. Running Python2 version."
+						python ../scripts/check_p2.py
+					fi
+						if grep -Fxq "$preffix-p$p-t$t" ../iters.txt
+						then
+						    # code if found
+						    echo "Simulation found in log. Skipping..."
+							else
+							    # code if not found
+							    echo "Not found. Simulating..."
 
-								# UNCOMMENT WHEN DONE WITH TESTING
-								export FILE="OUT/p$p-t$t/3-md"
-								# Modify 0_md file
-								# remove last two lines from 0_md file
-								echo -e '$d\nw\nq'| ed MDP/0_md.mdp
-								echo -e '$d\nw\nq'| ed MDP/0_md.mdp
+									# UNCOMMENT WHEN DONE WITH TESTING
+									export FILE="OUT/p$p-t$t/3-md"
+									# Modify 0_md file
+									# remove last two lines from 0_md file
+									echo -e '$d\nw\nq'| ed MDP/0_md.mdp
+									echo -e '$d\nw\nq'| ed MDP/0_md.mdp
 
-								# write pressure and temperature iterations
-								echo "ref_p 		=" $p>>MDP/0_md.mdp
-								echo "ref_t 		=" $t>>MDP/0_md.mdp
+									# write pressure and temperature iterations
+									echo "ref_p 		=" $p>>MDP/0_md.mdp
+									echo "ref_t 		=" $t>>MDP/0_md.mdp
 
-								# Modify 0_npt file
-								# remove last two lines from 0_npt file
-								echo -e '$d\nw\nq'| ed MDP/0_npt.mdp
-								echo -e '$d\nw\nq'| ed MDP/0_npt.mdp
+									# Modify 0_npt file
+									# remove last two lines from 0_npt file
+									echo -e '$d\nw\nq'| ed MDP/0_npt.mdp
+									echo -e '$d\nw\nq'| ed MDP/0_npt.mdp
 
-								# write pressure and temperature iterations
-								echo "ref_p 		=" $p>>MDP/0_npt.mdp
-								echo "ref_t 		=" $t>>MDP/0_npt.mdp
+									# write pressure and temperature iterations
+									echo "ref_p 		=" $p>>MDP/0_npt.mdp
+									echo "ref_t 		=" $t>>MDP/0_npt.mdp
 
-								# Modify 0_nvt file
-								# remove last lines from 0_nvt file
-								echo -e '$d\nw\nq'| ed MDP/0_nvt.mdp
+									# Modify 0_nvt file
+									# remove last lines from 0_nvt file
+									echo -e '$d\nw\nq'| ed MDP/0_nvt.mdp
 
-								# write temperature iterations
-								echo "ref_t 		=" $t>>MDP/0_nvt.mdp
+									# write temperature iterations
+									echo "ref_t 		=" $t>>MDP/0_nvt.mdp
 
-								# make temporary folder to store simulated variables
-								if mkdir OUT/p$p-t$t
-									then
-										echo "Making folder"
-										# UNCOMMENT AFTER TESTS
-										run-commands
-										echo "current folder is"
-										pwd
-									else
-										echo "Folder exists... Skipping to radial distribution function"
-									fi
-										gas-gas
-										# send to main network computer
-										scp -P 28 -r -C /home/test/lennard-jones-sims/results/rdf-$preffix-p$p-t$t.xvg test@148.247.198.140:/home/test/git/lennard-jones-sims/results
-										cleanup
-										stash
-						fi
+									# make temporary folder to store simulated variables
+									if mkdir OUT/p$p-t$t
+										then
+											echo "Making folder"
+											# UNCOMMENT AFTER TESTS
+											run-commands > ../logs/sim-$preffix-p$p-t$t.log
+											echo "current folder is"
+											pwd
+										else
+											echo "Folder exists... Skipping to radial distribution function"
+										fi
+											gas-gas > ../logs/rdf-$preffix-p$p-t$t.log 2>&1 &
+							fi
+					done
 				done
-			done
-			cd ..
-	done
-}
+				cd ..
+		done
+	}
 
-stash(){
-	# finally update with remote files
-	git add /home/test/lennard-jones-sims/results/
-	git stash
-	git pull
-}
+	submit(){
+		git add ../results/rdf-$preffix-p$p-t$t.xvg
+		git add ../logs/rdf-$preffix-p$p-t$t.log
+		git add ../logs/sim-$preffix-p$p-t$t.log
+		git commit -m "feat: add $preffix-p$p-t$t radial distribution function"
 
-cleanup(){
-	# remove duplicate mdout files
-	rm *mdout.mdp*
-	# write pressure and temperature in iteration log
-	echo $preffix-p$p-t$t >> ../iters.txt
-	rm -r OUT/p$p-t$t
-}
+		git add ../logs
+		git commit -m "add commit head to log backlog"
+		# Always use branch computer alias
+		# make sure to have a branch for each
+		git push origin dev
+	}
 
 	gas-gas(){
 		echo "del 0-1" > input
@@ -116,8 +116,11 @@ cleanup(){
 		echo "0" > input
 		echo "0" >> input
 		echo "calculating radial distribution function"
-		gmx rdf -f $FILE.trr -s $FILE.tpr -n indexrdf.ndx -bin 0.001 -rmax 3.9  -b $BEGMD -e $ENDMD -o ../results/rdf-$preffix-p$p-t$t.xvg < input
+		gmx rdf -f $FILE.trr -s $FILE.tpr -n indexrdf.ndx -bin 0.001 -rmax 2.0 -o ../results/rdf-$preffix-p$p-t$t.xvg < input
 		rm indexrdf.ndx
+		rm *.ndx*
+		rm -r OUT/p$p-t$t
+		submit
 	}
 
 
