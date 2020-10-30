e21cccc3b39c2ef7b28a3f4c89ace09fbe664b03
root <root@localhost.localdomain>
2020-10-30 13:35:28 -0500

    fix path error in check_p2

--- scripts/check_p2.py
+++ scripts/check_p2.py
@@ -4,8 +4,8 @@
 # python 2 version of check_files.py
 import os
 
-with open("iters.txt","w+") as f:
-    for root, dirs, files in os.walk("results"):
+with open("../iters.txt","w+") as f:
+    for root, dirs, files in os.walk("../results"):
         for name in files:
             print(name[4:-4])
             f.write(name[4:-4])
