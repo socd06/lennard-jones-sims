02af6c372cf8e07145b1cff453ac5ba2e642b9bf
Carlos Salgado <socd06@gmail.com>
2020-11-02 10:52:21 -0500

    Update beo.sh
    add branch tracking in scripts

--- beo.sh
+++ beo.sh
@@ -95,13 +95,17 @@ run-folders() {
 	}
 
 	submit(){
+		branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+		echo $preffix-p$p-t$t >>../iters-$branch.txt
+		echo "Working in $branch branch"
+		git add ../iters-$branch.txt
 		git add ../results/rdf-$preffix-p$p-t$t.xvg
 		git add ../logs
 		git commit -m "feat: add $preffix-p$p-t$t radial distribution function and updates to logs"
 		# Always use branch computer alias
 		# make sure to have a branch for each
-		git push -u origin beo
-	}
+		git push -u origin $branch
+		}
 
 	gas-gas(){
 		echo "del 0-1" > input
